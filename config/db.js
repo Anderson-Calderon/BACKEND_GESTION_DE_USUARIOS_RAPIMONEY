4963827dd18ccf1ee3271e9398b0f59b794e8fdb
Anderson-Calderon <anderson.calderoncampos@gmail.com>
2024-01-11 17:10:18 -0500

    dejamos de trabajar con rutas absolutas y optamos por trabajar con rutas relativas en el archivo db.js

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
 // Import the sqlite3 module
 import sqlite3 from "sqlite3";
-import path from "path";
+
 // Function to connect to the database
 const connectDB = ()=>{
 
-    const dbPath = path.resolve(__dirname, '../rapimoney-db.db');
+  
     // Create a database instance with the file "rapioney-db.db"
-    const db = new sqlite3.Database(dbPath,sqlite3.OPEN_READWRITE , (err)=>{
+    const db = new sqlite3.Database('./rapimoney-db.db',sqlite3.OPEN_READWRITE , (err)=>{
 
         // Handle connection errors
         if(err){
